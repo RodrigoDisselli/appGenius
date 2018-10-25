c444494618beea9802a3357cc5f30151cb9f468d
Olaf <disselli.rodrigo@gmail.com>
2018-10-24 21:22:36 -0300

    conexão com a antena bluetooth aula parte 5 e 6

--- Genius/app/src/main/java/com/example/themarceneiro/genius/MainActivity.java
+++ Genius/app/src/main/java/com/example/themarceneiro/genius/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.themarceneiro.genius;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import android.view.View.OnClickListener;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
     //declaração para o bluetooth
     BluetoothAdapter myBluetooth = null;
     BluetoothDevice myDevice = null;
+    BluetoothSocket mySocket = null;
+
+    UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     private static final int ENABLED_BLUETOOTH = 1;
     private static final int CONECTION_BLUETOOTH = 2;
-    boolean conection = false;
+    boolean connection = false;
+
+    private static String MAC;
 
 
     @Override
@@ -90,27 +97,22 @@ public class MainActivity extends AppCompatActivity {
 
     //metodo para mostrar dispositivos disponiveis para conexão
     public void enabledConection(){
-        if(conection){
+        if(connection){
+
+            try {
+
+                mySocket.close();
+                connection = false;
+
+            } catch (IOException erro) {
+
+            }
 
         }else {
             Intent openListaDipositivos = new Intent(MainActivity.this, ListaDispositivos.class);
             startActivityForResult(openListaDipositivos, CONECTION_BLUETOOTH);
         }
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode)
-        {
-            case CONECTION_BLUETOOTH:
-                if (resultCode == Activity.RESULT_OK) {
-                }else {
-                }
-                break;
-        }
-    }
-
 
     public void turnOnBluetooth(){
         if(myBluetooth == null){
@@ -126,5 +128,39 @@ public class MainActivity extends AppCompatActivity {
             //abre a janela de ativação do bluetooth do aparelho
         }
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case CONECTION_BLUETOOTH:
+                if (resultCode == Activity.RESULT_OK) {
+                    MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
+                    Toast.makeText(getApplicationContext(), String.valueOf(MAC), Toast.LENGTH_SHORT).show();
+                    myDevice = myBluetooth.getRemoteDevice(MAC);
+
+                    try {
+
+                        mySocket = myDevice.createRfcommSocketToServiceRecord(MY_UUID);
+
+                        mySocket.connect();
+
+                        connection = true;
+
+                        connectBtn.setText("Disconnect");
+
+                        Toast.makeText(getApplicationContext(), "Você está conectado com " + MAC, Toast.LENGTH_SHORT).show();
+
+                    } catch (IOException erro) {
+                        Toast.makeText(getApplicationContext(), "Erro ao conectar", Toast.LENGTH_SHORT).show();
+                    }
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Falha ao obter o MAC!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
